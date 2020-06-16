6150bdf5becc5b81d30243d41543cd3fb64eedca
xonix <xonixx@gmail.com>
2020-06-16 21:09:15 +0300

    Initial version #1

--- src/main/java/com/github/jsqry/cli/App.java
+++ src/main/java/com/github/jsqry/cli/App.java
@@ -1,7 +1,13 @@
 package com.github.jsqry.cli;
 
+import org.graalvm.polyglot.Context;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello Java!");
+
+    try (Context context = Context.create()) {
+      context.eval("js", "print('Hello JavaScript!');");
+    }
   }
 }
