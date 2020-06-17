2d298d194bf95fd3877cbb4bbaa30515b3fbaff4
xonix <xonixx@gmail.com>
2020-06-17 13:24:44 +0300

    Initial version #1

--- src/main/java/com/github/jsqry/cli/App.java
+++ src/main/java/com/github/jsqry/cli/App.java
@@ -5,6 +5,7 @@ import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
 
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -13,8 +14,7 @@ public class App {
   public static void main(String[] args) {
     //    System.out.println("Hello Java!");
 
-    URL jsFileResource =
-        Thread.currentThread().getContextClassLoader().getResource("jsqry.js");
+    URL jsFileResource = Thread.currentThread().getContextClassLoader().getResource("jsqry.js");
 
     if (jsFileResource == null) {
       throw new IllegalStateException("no js lib file");
@@ -22,12 +22,32 @@ public class App {
 
     String jsFileContent = new String(Files.readAllBytes(Paths.get(jsFileResource.toURI())));
 
+    String inputJsonStr = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
+
     try (Context context = Context.create()) {
       //      context.eval("js", "print('Hello JavaScript!');");
 
       context.eval("js", jsFileContent);
-      Value queryFunction = context.eval("js", "jsqry.query");
-      System.out.println(queryFunction);
+
+      //      Value queryFunction = context.eval("js", "jsqry.query");
+      //      System.out.println(queryFunction);
+
+      Value logicFunction =
+          context.eval(
+              "js",
+              "(jsonStr, queryStr) => { "
+                  + "let json;"
+                  + "try {"
+                  + " json = JSON.parse(jsonStr);"
+                  + "} catch (e) {"
+                  + " print('Wrong JSON');"
+                  + " return"
+                  + "}"
+                  + "const res = jsqry.query(json, queryStr);"
+                  + "print(JSON.stringify(res, null, 2));"
+                  + " }");
+
+      logicFunction.executeVoid(inputJsonStr, args[0]);
     }
   }
 }
---
+++ test_app.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+#set -x
+
+GRAAL=~/soft/graalvm-ce-java11-20.1.0
+UPX=~/soft/upx-3.95-amd64_linux
+
+graal_version=$($GRAAL/bin/java -version 2>&1 | grep '64' | grep GraalVM | sed -E 's#.+(GraalVM.+) \(.+#\1#')
+java_version=$($GRAAL/bin/java -version 2>&1 | head -n 1 | sed 's/ version//' | sed 's/"//g')
+
+echo "Java version: $java_version"
+echo "Graal version: $graal_version"
+
+
+JAVA="$GRAAL/bin/java"
+
+CP=$(mvn -q exec:exec -Dexec.executable=echo -Dexec.args="%classpath")
+echo $CP
+
+echo '[{"a":1},{"a":2}]' | $JAVA -cp "$CP" com.github.jsqry.cli.App "a"
+echo 'wrong' | $JAVA -cp "$CP" com.github.jsqry.cli.App "a"
