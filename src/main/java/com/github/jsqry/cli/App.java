d3c1870d605911509714b0c4cd2576cde25463d8
xonix <xonixx@gmail.com>
2020-06-19 18:17:27 +0300

    Add -v CLI option #6

--- src/main/java/com/github/jsqry/cli/App.java
+++ src/main/java/com/github/jsqry/cli/App.java
@@ -1,6 +1,7 @@
 package com.github.jsqry.cli;
 
 import lombok.SneakyThrows;
+import org.apache.commons.cli.*;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Value;
 
@@ -11,10 +12,43 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
+
   @SneakyThrows
   public static void main(String[] args) {
-    String inputJsonStr = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
-    String query = args.length == 0 ? "" : args[0];
+    Options options = new Options();
+
+    Option version = new Option("v", "version", false, "print version and exit");
+    version.setRequired(false);
+    options.addOption(version);
+
+    Option help = new Option("h", "help", false, "print help and exit");
+    help.setRequired(false);
+    options.addOption(help);
+
+    CommandLineParser parser = new DefaultParser();
+    CommandLine cmd;
+
+    try {
+      cmd = parser.parse(options, args);
+    } catch (ParseException e) {
+      throw printHelpAndExit(e.getMessage(), options);
+    }
+
+    if (cmd.hasOption(help.getLongOpt())) {
+      System.out.println(Constants.UTILITY_NAME + " ver. " + Constants.getFullVersion());
+      HelpFormatter formatter = new HelpFormatter();
+      formatter.printHelp(Constants.UTILITY_HELP_LINE, options);
+      System.exit(0);
+    }
+
+    if (cmd.hasOption(version.getLongOpt())) {
+      System.out.println(Constants.getFullVersion());
+      System.exit(0);
+    }
+
+    List<String> argList = cmd.getArgList();
+
+    String query = argList.isEmpty() ? "" : argList.get(0);
 
     List<String> scripts = new ArrayList<>();
 
@@ -41,7 +75,18 @@ public class App {
 
       Value doWork = context.eval("js", "doWork");
 
+      String inputJsonStr = new String(System.in.readAllBytes(), StandardCharsets.UTF_8);
       doWork.executeVoid(inputJsonStr, query);
     }
+  }
+
+  private static IllegalStateException printHelpAndExit(String message, Options options) {
+    if (message != null) {
+      System.err.println(message);
+    }
+    HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp(Constants.UTILITY_HELP_LINE, options);
+    System.exit(1);
+    return new IllegalStateException();
   }
 }
--- src/main/java/com/github/jsqry/cli/Constants.java
+++ src/main/java/com/github/jsqry/cli/Constants.java
@@ -1,6 +1,9 @@
 package com.github.jsqry.cli;
 
 class Constants {
+  public static final String UTILITY_NAME = "jsqry";
+  public static final String UTILITY_HELP_LINE = "echo $JSON | " + UTILITY_NAME + " 'query'";
+
   private static final String VERSION = "%APP_VERSION%";
   private static final String VERSION_EXT = " (%GRAAL_VERSION%, %JAVA_VERSION%)";
 
